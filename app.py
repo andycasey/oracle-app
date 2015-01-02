57d9aaf83b524d93ad5825e5028d8cdfadb8aee2
Andy Casey <andycasey@gmail.com>
2015-01-02 07:10:08 +0000

    fix

--- app.py
+++ app.py
@@ -18,9 +18,11 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 
 # Automatically tear down SQLAlchemy.
+'''
 @app.teardown_request
 def shutdown_session(exception=None):
     db_session.remove()
+'''
 
 # Login required decorator.
 def login_required(test):
