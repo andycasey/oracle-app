7cd9855d6ba338e5f5fb17d325675b56f8ed8db4
Andy Casey <andycasey@gmail.com>
2015-01-02 06:59:35 +0000

    Specify a port manually

--- app.py
+++ app.py
@@ -95,12 +95,10 @@ if not app.debug:
 #----------------------------------------------------------------------------#
 
 # Default port:
-if __name__ == '__main__':
-    app.run()
+#if __name__ == '__main__':
+#    app.run()
 
 # Or specify port manually:
-'''
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-'''
